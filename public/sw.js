e33952e5d3d4ab254eaf733e84939d6eb39a7936
hunviettd-75 <hunviettd@mail.com>
2026-04-26 14:50:09 +0700

    Bien ung dung thanh PWA de xoa logo Chrome tren icon

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Minimal fetch handler to satisfy PWA requirements
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
